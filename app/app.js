91c14cf653df20756b6c1d273b6535d10fc42d7b
Julien M <le2riste@hotmail.com>
2017-06-28 10:17:55 +0200

    Exemple angularJs

---
+++ app/app.js
@@ -0,0 +1,40 @@
+// import './css/skel.css';
+// import './css/style.css';
+// import './css/style-xlarge.css';
+
+import 'angular/angular-csp.css';
+import './style.scss';
+
+import 'angular';
+import './ors-star/ors-star.js';
+
+var app = angular.module('main', ['ors-star']);
+
+import orsHeaderUrl from './tmpl/ors-header.html';
+// const orsHeaderUrl = require('./tmpl/ors-header.html');
+
+const orsBodyUrl = require('./tmpl/ors-body.html');
+const orsFooterUrl = require('./tmpl/ors-footer.html');
+
+app.directive('orsHeader', function () {
+	return {
+		restrict: 'E',
+		template: orsHeaderUrl
+	};
+});
+
+app.directive('orsBody', function () {
+	return {
+		restrict: 'E',
+		template: orsBodyUrl
+	};
+});
+
+app.directive('orsFooter', function () {
+	return {
+		restrict: 'E',
+		template: orsFooterUrl
+	};
+});
+
+
---
+++ app/ors-star/ors-star.js
@@ -0,0 +1,41 @@
+(function () {
+	'use strict';
+
+	var app = angular.module('ors-star', []);
+
+	app.directive('orsStar', function () {
+		return {
+			restrict: 'E',
+			scope: {
+				note: '=?'
+			},
+			controller: function OrsStarCtrl($scope, $element, $compile) {
+				'ngInject';
+				console.log('OrsStarCtrl', arguments);
+				$scope.update = function update(newNote) {
+					console.log('newNote', newNote);
+					$scope.note = newNote;
+				};
+				$scope.$watch('note', function () {
+					let note = $scope.note || 3;
+					note = (note > 5) ? 5 : note;
+					note = (note < 0) ? 0 : note;
+					let html = '';
+					for (let i = 0; i < note; i++) {
+						html += '<img ng-click="update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png">';
+					}
+					for (let i = note; i < 5; i++) {
+						html += `
+<img  
+ng-click="update(${i + 1})" 
+src="./ors-star/img/white_star.png">`;
+					}
+					$element.html(html);
+					$compile($element.contents())($scope);
+				});
+
+			}
+		};
+	});
+
+})();
